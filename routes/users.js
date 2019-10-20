bd5b5a0f68ddb2831a8be4fae83d78b801798838
med2bouanane <med2bouanane@gmail.com>
2019-10-20 18:54:20 +0200

    add swagger with open api specification

--- routes/users.js
+++ routes/users.js
@@ -8,17 +8,24 @@ router.get("/:id", userController.getById);
 router.get("/username/:username", userController.getByUserName);
 router.get("/email/:email", userController.getByEmail);
 
-// POST
-router.post("/", userController.create);
-
 // PUT
 router.put("/:id", userController.update);
+router.put("/username/:username", userController.update);
+router.put("/email/:email", userController.update);
 
 // DELETE
 router.delete("/:id", userController.remove);
+router.delete("/username/:username", userController.remove);
+router.delete("/email/:email", userController.remove);
 
 // PATCH
-router.patch('/:id', userController.update)
+router.patch('/:id', userController.update);
+router.patch('/:username', userController.update);
+router.patch('/:email', userController.update);
+
+// POST
+router.post("/", userController.create);
+router.post("/some", userController.createSome);
 
 
 router.post("/login", userController.login);
